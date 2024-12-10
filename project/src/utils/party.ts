f398d263249c874c5e160f32321b971b3e7eaad7
Michelingumman <adam.michelin01@gmail.com>
2024-12-10 20:20:44 +0100

    leaving the gamesettings for now

--- project/src/utils/party.ts
+++ project/src/utils/party.ts
@@ -1,6 +1,7 @@
 import { GAME_CONFIG } from '../config/gameConfig';
 
 export const generatePartyCode = (): string => {
+  
   return Math.floor(Math.random() * (10 ** GAME_CONFIG.PARTY_CODE_LENGTH)).toString().padStart(GAME_CONFIG.PARTY_CODE_LENGTH, '0');
 };
 
