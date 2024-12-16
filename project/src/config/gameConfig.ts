98bc13331171e77687bc5fad4a0d4b1f1205bcd9
Michelingumman <adam.michelin01@gmail.com>
2024-12-16 12:31:57 +0100

    balancing initial health

--- project/src/config/gameConfig.ts
+++ project/src/config/gameConfig.ts
@@ -8,8 +8,8 @@ export const GAME_CONFIG = {
   // Player stats (now using floating points)
   INITIAL_HEALTH: 20.0,
   INITIAL_MANA: 10.0,
-  MAX_HEALTH: 10.0,
-  MAX_MANA: 10.0,
+  MAX_HEALTH: 20.0,
+  MAX_MANA: 20.0,
   MANA_DRINK_AMOUNT: 10.0,
 
 } as const;
